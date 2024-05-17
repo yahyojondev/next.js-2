9dd2c4012b9ba26f2128222e30b98c21f0146633
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-17 08:03:46 +0500

    work

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "./components/hero/Hero";
-import Page from "@/app/product/page";
+// import Page from "@/app/product/page";
+import Product from "./product/Product";
 
 export default function Home() {
   return (
     <main className="home">
       <Hero />
-      <Page />
+      <Product />
     </main>
   );
 }
